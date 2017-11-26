164d5bf6929b67c31cc305ce0ef6210b66775380
MSI\IRWAN <fauzancahya16@gmail.com>
2017-11-27 00:29:25 +0700

    no message

--- application/views/accounts_v.php
+++ application/views/accounts_v.php
@@ -17,7 +17,7 @@
                             <li class="breadcrumb-item">
                                 <a href="#">Accounts</a>
                             </li>
-                        </ol>
+                        </ol> 
                     </div>
                     </div>
                 </div>
