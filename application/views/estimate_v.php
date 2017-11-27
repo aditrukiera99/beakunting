24bfa10c9fdadd73699b31bf073120d5cbf40556
MSI\IRWAN <fauzancahya16@gmail.com>
2017-11-27 15:03:17 +0700

    aa

---
+++ application/views/estimate_v.php
@@ -0,0 +1,296 @@
+<header class="head">
+                <div class="main-bar">
+                    <div class="row">
+                    <div class="col-sm-4">
+                        <h4 class="nav_top_align">
+                            <i class="fa fa-inbox"></i>
+                            ESTIMATES
+                        </h4>
+                    </div>
+                    <div class="col-sm-8">
+                        <ol class="breadcrumb float-right  nav_breadcrumb_top_align">
+                            <li class="breadcrumb-item">
+                                <a href="index.html">
+                                    <i class="fa fa-home" data-pack="default" data-tags=""></i>
+                                    Dashboard
+                                </a>
+                            </li>
+                            <li class="breadcrumb-item">
+                                <a href="#">Email</a>
+                            </li>
+                            <li class="active breadcrumb-item">Estimates</li>
+                        </ol>
+                    </div>
+                    </div>
+                </div>
+            </header>
+   <br>
+<div class="row">
+    <div class="col-lg-12">
+        <div class="card">
+            <div class="card-header bg-primary">ESTIMATES</div>
+            <div class="card-block cards_section_margin">
+                
+                <div class="card-block">
+                    <br>
+                    <div class="row">
+                        <div class="col-lg-6">
+                                        <form class="form-horizontal">
+                                            <fieldset>
+                                                <div class="form-group row">
+                                                    <div class="col-lg-3 col-xl-3 text-lg-right">
+                                                        <label for="name4" class=" col-form-label">Customer Name</label>
+                                                    </div>
+                                                    <div class="col-lg-8 col-xl-8">
+                                                        <div class="input-group">
+                                                            <span class="input-group-addon">
+                                                                <i class="fa fa-users"></i>
+                                                            </span>
+                                                            <select id="gender4" class="form-control">
+                                                                <option>Pilih Costumer</option>
+                                                                <option>Male</option>
+                                                                <option>Female</option>
+                                                            </select>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="form-group row">
+                                                    <div class="col-lg-3 col-xl-3 text-lg-right">
+                                                        <label for="username4" class="col-form-label">Tanggal</label>
+                                                    </div>
+                                                    <div class="col-lg-8 col-xl-4">
+                                                        <div class="input-group">
+                                                        <span class="input-group-addon">
+                                                            <i class="fa fa-calendar-o"></i>
+                                                        </span>
+                                                            <input type="date" id="username4" class="form-control" placeholder="Date">
+                                                        </div>
+                                                    </div>
+                                                    
+                                                </div>
+                                                <div class="form-group row">
+                                                    <div class="col-lg-3 col-xl-3 text-lg-right">
+                                                        <label for="password4" class="col-form-label">Kode</label>
+                                                    </div>
+                                                    <div class="col-lg-8 col-xl-4">
+                                                        <div class="input-group">
+                                                            <span class="input-group-addon">
+                                                                <i class="fa fa-tags"></i>
+                                                            </span>
+                                                            <input type="text" id="password4" class="form-control" placeholder="Kode Invoice">
+                                                        </div>
+                                                    </div>
+                                                    
+                                                </div>
+                                                <div class="form-group row">
+                                                    <div class="col-lg-3 col-xl-3 text-lg-right">
+                                                        <label for="username4" class="col-form-label">Tanggal Pembayaran</label>
+                                                    </div>
+                                                    <div class="col-lg-8 col-xl-4">
+                                                        <div class="input-group">
+                                                        <span class="input-group-addon">
+                                                            <i class="fa fa-calendar-o"></i>
+                                                        </span>
+                                                            <input type="date" id="username4" class="form-control" placeholder="Date">
+                                                        </div>
+                                                    </div>
+                                                    
+                                                </div>
+                                                
+                                                <!-- last name-->
+                                                
+                                                
+                                            </fieldset>
+                                        </form>
+                                    </div>
+                                   
+                                    <div class="col-lg-6">
+                                        
+                                        <div class="form-group row">
+                                            <div class="col-lg-3 col-xl-3 text-lg-right">
+                                                <label for="username4" class="col-form-label">Alamat Pembayaran</label>
+                                            </div>
+                                                <div class="col-lg-8 col-xl-8">
+                                                    <textarea class="form-control" id="query1" rows="4" placeholder="Alamat Pembayaran"></textarea>
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <div class="col-lg-3 col-xl-3 text-lg-right">
+                                                <label for="username4" class="col-form-label">Alamat Pengiriman</label>
+                                            </div>
+                                                <div class="col-lg-8 col-xl-8">
+                                                    <textarea class="form-control" id="query1" rows="4" placeholder="Alamat Pengiriman"></textarea>
+                                            </div>
+                                        </div>
+                                        </div>
+                                    </div>
+                                
+                                </div>
+
+                                <table class="table table-bordered table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th class="bg-primary">ITEM</th>
+                                            <th class="bg-primary">DESCRIPTION</th>
+                                            <th class="bg-primary">QUANTITY</th>
+                                            <th class="bg-primary">U/M</th>
+                                            <th class="bg-primary">RATE</th>
+                                            <th class="bg-primary">AMOUNT</th>
+                                            <th class="bg-primary">TAX</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+
+                                        <tr>
+                                            <td>Kampret</td>
+                                            <td>0.00</td>
+                                            <td>Kampret</td>
+                                            <td>0.00</td>
+                                            <td>Kampret</td>
+                                            <td>0.00</td>
+                                            <td>0.00</td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>Kampret</td>
+                                            <td>0.00</td>
+                                            <td>Kampret</td>
+                                            <td>0.00</td>
+                                            <td>Kampret</td>
+                                            <td>0.00</td>
+                                            <td>0.00</td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>Kampret</td>
+                                            <td>0.00</td>
+                                            <td>Kampret</td>
+                                            <td>0.00</td>
+                                            <td>Kampret</td>
+                                            <td>0.00</td>
+                                            <td>0.00</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                                                                           
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <div class="form-group row">
+                                        <div class="col-lg-3 col-xl-3 text-lg-right">
+                                            <label for="name4" class=" col-form-label">Online Pay</label>
+                                        </div>
+                                        <div class="col-lg-8 col-xl-8">
+                                            <div class="input-group">
+                                                <span class="input-group-addon">
+                                                    <i class="fa fa-tags"></i>
+                                                </span>
+                                                <select id="gender4" class="form-control">
+                                                    <option>Pilih.....</option>
+                                                    <option>OFF</option>
+                                                    <option>Female</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                            <div class="col-lg-3 col-xl-3 text-lg-right">
+                                                <label for="username4" class="col-form-label">Customer Message</label>
+                                            </div>
+                                                <div class="col-lg-8 col-xl-8">
+                                                    <div class="input-group">
+                                                    <span class="input-group-addon">
+                                                        <i class="fa fa-envelope"></i>
+                                                    </span>
+                                                    <textarea class="form-control" id="query1" rows="3" placeholder="Type Text Here...."></textarea>
+                                                </div>
+                                            </div>
+                                        </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="form-group row">
+                                        <div class="col-lg-3 col-xl-1 text-lg-right">
+                                            <label for="name4" class=" col-form-label">Tax </label> 
+                                        </div>
+                                        <div class="col-lg-8 col-xl-3">
+                                            <select id="gender4" class="form-control">
+                                                    <option>Pilih.....</option>
+                                                    <option>OFF</option>
+                                                    <option>Female</option>
+                                                </select>
+                                        </div>
+                                        <div class="col-lg-3 col-xl-1 text-lg-right">
+                                            <label for="name4" class=" col-form-label">(60.5%) </label> 
+                                        </div>
+                                        <div class="col-lg-3 col-xl-4 text-lg-right">
+                                            <label for="name4" class=" col-form-label">0.00</label> 
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <div class="col-lg-3 col-xl-5 text-lg-right">
+                                            <label for="name4" class=" col-form-label">Total </label> 
+                                        </div>
+                                        
+                                        
+                                        <div class="col-lg-3 col-xl-4 text-lg-right">
+                                            <label for="name4" class=" col-form-label">0.00</label> 
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <div class="col-lg-3 col-xl-5 text-lg-right">
+                                            <label for="name4" class=" col-form-label">Payment Aplied </label> 
+                                        </div>
+                                        
+                                        
+                                        <div class="col-lg-3 col-xl-4 text-lg-right">
+                                            <label for="name4" class=" col-form-label">0.00</label> 
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <div class="col-lg-3 col-xl-5 text-lg-right">
+                                            <label for="name4" class=" col-form-label">Balance Due </label> 
+                                        </div>
+                                        
+                                        
+                                        <div class="col-lg-3 col-xl-4 text-lg-right">
+                                            <label for="name4" class=" col-form-label" style="font-weight: bold;font-size: 25px;">0.00</label> 
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <div class="col-lg-12 col-xl-12 text-lg-right">
+                                            
+                                        </div>
+                                        
+                                        
+                                        <div class="col-lg-6 col-xl-6 text-lg-right">
+                                            <button type="button" class="btn btn-labeled btn-success">
+                                                <span class="btn-label"><i class="fa fa-save"></i></span>
+                                                Simpan
+                                            </button>
+                                        </div>
+                                         <div class="col-lg-3 col-xl-3 text-lg-right"> 
+                                            <button type="button" class="btn btn-labeled btn-primary">
+                                                <span class="btn-label"><i class="fa fa-times"></i></span>
+                                                Batal
+                                            </button>
+                                        </div>
+                                    </div>
+
+                                </div>
+                                    
+                            </div>
+                                
+                            </div>
+
+                        
+            </div>
+        </div>
+    </div>
+
+</div>
+        
