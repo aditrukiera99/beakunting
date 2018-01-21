43137fa15509ceab59eb4d9a40980034ffb31fac
MSI\IRWAN <fauzancahya16@gmail.com>
2018-01-21 18:50:26 +0700

    no message

--- application/views/company_v.php
+++ application/views/company_v.php
@@ -32,7 +32,7 @@
                 <div class="row">
                     <div class="col-lg-9">
                         <div class="card">
-                            <div class="card-header bg-mint disabled">Product Information</div>
+                            <div class="card-header bg-mint disabled">Home</div>
                             <div class="card-block">
                                 <div class="form-group row">
 
@@ -107,7 +107,7 @@
                                     </div>
                                     <div class="col-lg-1" style="height:72px;background: url(<?=base_url();?>/assets/img/garisatas.png) no-repeat center;">
                                     </div>
-                                    <div class="col-lg-4 text-lg-right ">
+                                    <div class="col-lg-6 text-lg-right ">
                                         <img style="" src="<?=base_url();?>/assets/img/create sales reeipt.png">
                                     </div>
                                     
@@ -124,7 +124,7 @@
                                     </div>
                                     <div class="col-lg-1" style="height:50px;background: url(<?=base_url();?>/assets/img/garisatas.png) no-repeat center;">
                                     </div>
-                                    <div class="col-lg-3">
+                                    <div class="col-lg-5">
                                     </div>
                                     <div class="col-lg-1 " style="height:50px;background: url(<?=base_url();?>/assets/img/garisatas.png) no-repeat center;">
                                     </div>
@@ -134,16 +134,88 @@
                                     <div class="col-lg-1">
                                         
                                     </div>
-                                     <div class="col-lg-1" >
+                                    <div class="col-lg-1" >
                                         <img src="<?=base_url();?>/assets/img/estimates.png">
                                     </div>
                                     <div class="col-lg-1  text-lg-right" style="background: url(<?=base_url();?>/assets/img/garis.png) repeat-x center;">
                                     </div>
-                                    <div class="col-lg-1" style="height:50px;background: url(<?=base_url();?>/assets/img/garisatas.png) no-repeat center;">
+                                    <div class="col-lg-1" >
+                                        <img src="<?=base_url();?>/assets/img/create invoices.png">
                                     </div>
+                                    <div class="col-lg-4  text-lg-right" style="background: url(<?=base_url();?>/assets/img/garis.png) repeat-x center;">
+                                    </div>
+                                    <div class="col-lg-1" >
+                                        <img src="<?=base_url();?>/assets/img/receive payments.png">
+                                    </div>
+                                     <div class="col-lg-1  text-lg-right" style="background: url(<?=base_url();?>/assets/img/garis.png) repeat-x center;">
+                                    </div>
+                                    <div class="col-lg-1  text-lg-right" style="background: url(<?=base_url();?>/assets/img/garis.png) repeat-x center;">
+                                    </div>
+                                </div>
+
+                                <div class="form-group row" style="margin-bottom: 0rem !important;">
                                     <div class="col-lg-3">
                                     </div>
-                                    <div class="col-lg-1 " style="height:50px;background: url(<?=base_url();?>/assets/img/garisatas.png) no-repeat center;">
+                                    <div class="col-lg-1" style="height:50px;background: url(<?=base_url();?>/assets/img/garisatas.png) no-repeat center;">
+                                    </div>
+                                    <div class="col-lg-4">
+                                    </div>
+                                    <div class="col-lg-1" style="height:50px;background: url(<?=base_url();?>/assets/img/garisatas.png) no-repeat center;">
+                                    </div>
+                                </div>
+
+                                <div class="form-group row" style="margin-bottom: 0rem !important;" >
+                                    <div class="col-lg-3">
+                                    </div>
+                                    <div class="col-lg-1" style="height:70px;background: url(<?=base_url();?>/assets/img/garisatas.png) no-repeat center;">
+                                    </div>
+                                    <div class="col-lg-1" >
+                                        <img src="<?=base_url();?>/assets/img/Statement charges.png">
+                                    </div>
+                                    <div class="col-lg-1" style="height:50px;background: url(<?=base_url();?>/assets/img/garis.png) no-repeat center;">
+                                    </div>
+                                    <div class="col-lg-1" >
+                                        <img src="<?=base_url();?>/assets/img/finance charges.png">
+                                    </div>
+                                    <div class="col-lg-1" style="height:50px;background: url(<?=base_url();?>/assets/img/garis.png) no-repeat center;">
+                                    </div>
+                                    <div class="col-lg-1" >
+                                        <img src="<?=base_url();?>/assets/img/statements.png">
+                                    </div>
+                                </div>
+
+                                <div class="form-group row" >
+                                    <div class="col-lg-3">
+                                    </div>
+                                    <div class="col-lg-1" style="height:50px;background: url(<?=base_url();?>/assets/img/garisatas.png) no-repeat center;">
+                                    </div>
+                                </div>
+
+                                <div class="form-group row" style="margin-bottom: 0rem !important;">
+                                    <div class="col-lg-1">
+                                    </div>
+                                    <div class="col-lg-1" >
+                                        <img src="<?=base_url();?>/assets/img/payroll center.png">
+                                    </div>
+                                    <div class="col-lg-1">
+                                    </div>
+                                    <div class="col-lg-1" >
+                                        <img src="<?=base_url();?>/assets/img/enter time.png">
+                                    </div>
+                                    <div class="col-lg-1" style="height:50px;background: url(<?=base_url();?>/assets/img/garis.png) no-repeat center;">
+                                    </div>
+                                    <div class="col-lg-1" >
+                                        <img src="<?=base_url();?>/assets/img/pay employee.png">
+                                    </div>
+                                    <div class="col-lg-1" style="height:50px;background: url(<?=base_url();?>/assets/img/garis.png) no-repeat center;">
+                                    </div>
+                                    <div class="col-lg-1" >
+                                        <img src="<?=base_url();?>/assets/img/pay liabilities.png">
+                                    </div>
+                                    <div class="col-lg-1" style="height:50px;background: url(<?=base_url();?>/assets/img/garis.png) no-repeat center;">
+                                    </div>
+                                    <div class="col-lg-1" >
+                                        <img src="<?=base_url();?>/assets/img/proces payroll.png">
                                     </div>
                                 </div>
 
@@ -153,23 +225,66 @@
                     </div>
                     <div class="col-lg-3">                        
                         <div class="card">
-                            <div class="card-header bg-mint disabled">Company Information</div>
+                            <div class="card-header bg-mint disabled">Company </div>
                             <div class="card-block">
-                                <div class="row">
-                                    <div class="col-lg-6">
-                                        <div class="form-group row">
-                                            <div class="col-lg-1  text-lg-right">
-                                                <img src="<?=base_url();?>/assets/img/purchase order.png">
-                                            </div>
-                                            <div class="col-lg-8">
-                                                 Blok E  
-                                            </div>
-                                        </div>
+                                <div class="form-group row">
+                                    <div class="col-lg-12"></div>
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-lg-6 text-lg-center" >
+                                        <img src="<?=base_url();?>/assets/img/chart of accounts.png">
+                                    </div>
+                                    <div class="col-lg-6 text-lg-center" >
+                                        <img src="<?=base_url();?>/assets/img/inventory activities.png">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-lg-6 text-lg-center" >
+                                        <img src="<?=base_url();?>/assets/img/items service.png">
+                                    </div>
+                                    <div class="col-lg-6 text-lg-center" >
+                                        <img src="<?=base_url();?>/assets/img/order checks.png">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-lg-6 text-lg-center" >
+                                        <img src="<?=base_url();?>/assets/img/calendar.png">
+                                    </div>
+                                    
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-lg-12 text-lg-center" style="height:50px;background: url(<?=base_url();?>/assets/img/garis.png) no-repeat center;">
+                                    </div>
+                                    
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-lg-6 text-lg-center" >
+                                        <img src="<?=base_url();?>/assets/img/record deposit.png">
+                                    </div>
+                                    <div class="col-lg-6 text-lg-center" >
+                                        <img src="<?=base_url();?>/assets/img/reconcile.png">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-lg-6 text-lg-center" >
+                                        <img src="<?=base_url();?>/assets/img/write checks.png">
+                                    </div>
+                                    <div class="col-lg-6 text-lg-center" >
+                                        <img src="<?=base_url();?>/assets/img/check register.png">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-lg-6 text-lg-center" >
+                                        <img src="<?=base_url();?>/assets/img/print check.png">
+                                    </div>
+                                    <div class="col-lg-6 text-lg-center" >
+                                        <img src="<?=base_url();?>/assets/img/enter credit.png">
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+
                     
                 </div>
                 <br>
